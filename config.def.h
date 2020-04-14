179faa10772aac9bbb01e2eb291956833d921708
Dallin Dahl <dallinjdahl@gmail.com>
2020-04-14 11:50:31 -0600

    adding gcc error output

--- config.def.h
+++ config.def.h
@@ -2,13 +2,18 @@
 	 /*web addresses*/\
 	X("https?://.+") \
 	Y("port web \\0") \
+	/*man files*/\
+	X("([a-zA-Z0-9\\-_]+)\\(([0-9a-zA-Z]+)\\),?") \
+	Y("port man \\2 \\1") \
 	 /*local include files*/\
 	X("#include \"([a-zA-Z0-9]+\\.h)\"") \
 	Y("port edit \\1") \
 	 /*standard include files*/\
 	X("#include <([a-zA-Z0-9]+\\.h)>") \
 	Y("port edit $(std_inc \\1)") \
+	 /*addressed files: should work with gcc error output*/\
+	X("(.+):([0-9]+):([0-9]+):.*") \
+	Y("port 2a_edit \\1 \\2 \\3") \
 	 /*addressed files: should work with grep -n */\
 	X("(.+):([0-9]+|(/|\\?).+).*") \
 	Y("port a_edit \\1 \\2")
-
